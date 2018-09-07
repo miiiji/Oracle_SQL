9d76ee648afe8ef0bd71eab2e6e4987091ab8719
miiiji <alwldmsrk@gmail.com>
2018-09-08 01:50:19 +0900

    table delete

---
+++ Insert Update Delete/delete.sql
@@ -0,0 +1,17 @@
+drop table emp01;
+
+create table emp01
+AS
+select * from emp;
+
+delete * from emp01;
+
+drop table emp01;
+
+create table emp01
+AS
+select * emp01;
+
+--사원번호가 7499인 사원의 정보를 삭제
+delete * FROM emp01
+WHERE empno = 7499;
